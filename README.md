6471f804fa188a9c3a64adbf3354461adc81961a
danielefongo <danielefongo@gmail.com>
2018-06-11 22:52:39 +0200

    Added a simple server with a single route

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ docker build -t myfirstdocker ./main
 
 Then run the container using this command:
 ```
-docker run -it myfirstdocker:latest 
+docker run -it -p 8080:80 myfirstdocker:latest
 ```
+
+Then open a browser and try these URLS:
+- ```http:localhost:8080/hello```
+- ```http:localhost:8080/hello?name=Pippo```
--- main/myapp.py
+++ main/myapp.py
@@ -1,1 +1,14 @@
-print("Hello world.")
+from flask import Flask
+from flask import request
+
+echoserver = Flask(__name__)
+
+@echoserver.route("/hello")
+def hello():
+    name = request.args.get('name', default="World")
+
+    html = "<h3>Hello {name}!</h3>"
+    return html.format(name = name)
+
+if __name__ == "__main__":
+    echoserver.run(host='0.0.0.0', port=80)
